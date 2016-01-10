11b40277794a3a8819dce7644e9b4ef693ea9855
Sebastian Wellmar <swellmar@hotmail.com>
2016-01-10 23:08:00 +0100

    Ändrat från Engelska till Svenska

--- PandaWeb/PandaWeb/Models/EducationPlan.cs
+++ PandaWeb/PandaWeb/Models/EducationPlan.cs
@@ -15,28 +15,34 @@ namespace PandaWeb.Models
         public int EducationId { get; set; }
 
         [Required(ErrorMessage = "Ett namn för Utbildningen krävs")]
-       // [StringLength(50, ErrorMessage = "{0} måste innehålla minst {2} tecken.", MinimumLength = 4)]
+        // [StringLength(50, ErrorMessage = "{0} måste innehålla minst {2} tecken.", MinimumLength = 4)]
+        [Display(Name = "Utbildning")]
         public string Name { get; set; }
 
 
-		//[Required(ErrorMessage = "Ett Start Datum krävs.")]
-  //      [DisplayFormat(DataFormatString="{0:yyyy/MM/dd}")]
+        //[Required(ErrorMessage = "Ett Start Datum krävs.")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name = "Start datum")]
         public DateTime StartDate { get; set; }
         //[Required(ErrorMessage = "Ett Slut Datum krävs.")]
-        //[DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        [Display(Name = "Slut datum")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime EndDate { get; set; }
+        [Display(Name = "Studietakt")]
         public string PaceOfStudy { get; set; }
+        [Display(Name = "Distans/På plats")]
         public string FormOfStudy { get; set; }
 
         //länktabell till UL-tabellen
         public virtual UL UL { get; set; }
 
         //länktabell till kurser
+        [Display(Name = "Kurser")]
         public ICollection<Course> Courses { get; set; }
 
         //länktabell till Utbildningsplan
         public ULDocuments ULDocuments { get; set; }
 
-		public ICollection<Users> UserId { get; set; }
-	}
+        public ICollection<Users> UserId { get; set; }
+    }
 }
--- PandaWeb/PandaWeb/Models/Users.cs
+++ PandaWeb/PandaWeb/Models/Users.cs
@@ -9,30 +9,34 @@ namespace PandaWeb.Models
     [Table("Users")]
     public partial class Users
     {
-		[Key]
+        [Key]
         public int UserID { get; set; }
 
-		[Requried(ErrorMessage = "Please provide username", AllowEmptyStrings = false)]
+        [Requried(ErrorMessage = "Please provide username", AllowEmptyStrings = false)]
+        [Display(Name = "Användarnamn")]
         public string Username { get; set; }
 
-		[Requried(ErrorMessage = "Please provide Password", AllowEmptyStrings = false)]
+        [Requried(ErrorMessage = "Please provide Password", AllowEmptyStrings = false)]
         [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
+        [Display(Name = "Lösenord")]
         [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must be atleast 8 char long.")]
         public string Password { get; set; }
 
-		[System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Confirm password does not match.")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Confirm password does not match.")]
         [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
+        [Display(Name = "Upprepa Lösenord")]
         public string ConfirmPassword { get; set; }
 
-		[Required(ErrorMessage = "Please provide full name", AllowEmptyStrings = false)]
+        [Required(ErrorMessage = "Please provide full name", AllowEmptyStrings = false)]
+        [Display(Name = "Fullständiga namn")]
         public string Fullname { get; set; }
 
-		[RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,3})$",
+        [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,3})$",
             ErrorMessage = "Please provide valid email id")]
         public string Email { get; set; }
+        [Display(Name = "Roll")]
+        public string Role { get; set; }
 
-		public string Role { get; set; }
-
-		//public ICollection<EducationPlan> EducationId { get; set; }
-	}
+        //public ICollection<EducationPlan> EducationId { get; set; }
+    }
 }
